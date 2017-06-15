87dbc7c74a71f04740657557ccf01a8b05d01d77
JaeHo Song <thruthesky@gmail.com>
2017-06-15 15:25:01 +0800

    update for client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 
 import {
-  //User, UserTest,
+  User,
+  // UserTest,
   //Category, CategoryTest, CATEGORIES,
   // Post, PostTest, POST,
   CATEGORY, CATEGORIES,
@@ -18,26 +19,30 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  category_error: string;
   category_id: string;
   category_name: string;
   categories: CATEGORIES = [];
 
+  post_error: string;
+
 
   // post create/edit form
-  postForm: POST = {
+  postForm = {
     categories: {},
     subject: '',
-    content: '',
-    sticky_forum: false,
-    sticky_all_forum: false
+    content: ''
   };
 
   constructor(
     // userTest: UserTest,
-    // public user: User,
+    public user: User,
     public forum: ForumService
   ) {
+
+
+
     // userTest.run();
 
     // categoryTest.run();
@@ -56,50 +61,61 @@ export class AppComponent {
 
 
 
-  // onClickLoginWithGoogle() {
+  onClickLoginWithGoogle() {
 
-  //   this.user.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-  //     .then((res) => {
-  //       console.log("success");
-  //       console.log(res);
-  //     })
-  //     .catch(e => {
-  //       console.log('error: ', e);
-  //     });
-
-
-  // }
+    this.user.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((res) => {
+        console.log("success");
+        console.log(res);
+      })
+      .catch(e => {
+        console.log('error: ', e);
+      });
 
 
-  // onClickLoginWithFacebook() {
+  }
 
-  //   this.user.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
-  //     .then((res) => {
-  //       console.log("facebook login success");
-  //       console.log(res);
-  //     })
-  //     .catch(e => {
-  //       console.log('error: ', e);
-  //     });
-  // }
+
+  onClickLoginWithFacebook() {
+
+    this.user.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then((res) => {
+        console.log("facebook login success");
+        console.log(res);
+      })
+      .catch(e => {
+        console.log('error: ', e);
+      });
+  }
 
 
   onClickCreateCategory() {
     console.log(`Create: ${this.category_name}`);
-    this.forum.createCategory({ id: this.category_id, name: this.category_name }, () => {
-      console.log("create ok")
-    }, e => {
-      console.error(e);
-    });
+
+    let category = { id: this.category_id, name: this.category_name };
+    this.forum.createCategory( category )
+      .then( id => {} )
+      .catch( e => console.log( e ) );
+
+
+    // this.forum.createCategory(category, () => {
+    //   console.log("create ok")
+    // }, e => {
+    //   console.error(e);
+    // });
 
   }
 
 
   getCategories() {
-    this.forum.getCategories((categories) => {
-      this.categories = categories;
-      console.log('categories:', categories);
-    }, e => console.error(e));
+    this.forum.getCategories()
+      .then( categories => this.categories )
+      .catch( e => this.category_error = e.message );
+
+    // this.forum.getCategories((categories) => {
+    //   this.categories = categories;
+    //   console.log('categories:', categories);
+    // }, e => console.error(e));
   }
 
   onClickCategoryEdit( id ) {
@@ -107,20 +123,39 @@ export class AppComponent {
     let c = this.categories.find( v => v.id == id );
     console.log(c);
 
-    this.forum.editCategory( { id: c.id, name: c['name'], description: c['description'] }, () => {
-      console.log("Updated");
-    }, e => console.error(e) );
+    let category = { id: c.id, name: c['name'], description: c['description'] };
+    this.forum.editCategory( category )
+      .then( category_id => {} )
+      .catch( e => this.category_error = e.message );
+
+
+
+
+    // this.forum.editCategory( { id: c.id, name: c['name'], description: c['description'] }, () => {
+    //   console.log("Updated");
+    // }, e => console.error(e) );
   }
 
   onSubmitPostForm() {
     console.log("Going to create a post: ", this.postForm );
-    this.forum.createPost( this.postForm, (post) => {
-      console.log("Post created: ", post);
-    }, e => console.log(e) );
+
+    this.postForm.categories = Object.keys( this.postForm.categories ); // convert category object to array.
+    this.forum.createPost( <POST>this.postForm )
+      .then( key => console.log(`post created with : ${key}`))
+      .catch( e => {
+        this.post_error = e.message;
+        console.error(e);
+       } );
   }
   
   onClickCategoryDelete( id ) {
-    this.forum.deleteCategory( id, () => console.log("Category deleted"), e => console.error(e) );
+    
+    this.forum.deleteCategory( id )
+      .then( () => {} )
+      .catch( e => this.category_error = e.message );
+
+    // this.forum.deleteCategory( id, () => console.log("Category deleted"), e => console.error(e) );
+
   }
 
   listenCategory() {
